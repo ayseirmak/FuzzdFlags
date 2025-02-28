652db96e7f7e321a78748907c2d19a41c70150bd
ayseirmak <152956281+ayseirmak@users.noreply.github.com>
2025-02-28 00:40:14 +0000

    Update setup_m510.sh

--- setup_m510.sh
+++ setup_m510.sh
@@ -141,34 +141,7 @@ sudo apt-get clean
 
 echo "All setup complete! Run: su - user42"
 
-# -------------------------------------------------------
-# Step 10: Download & build LLVM 17 with AFL compiler
-# -------------------------------------------------------
-cd ~
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout release/17.x
 
-# Create and enter a separate build directory
-mkdir ~/build-test
-cd ~/build-test
-
-# Configure LLVM build to use AFL's clang-fast
-LD=/usr/local/bin/afl-clang-fast++ cmake -G Ninja -Wall ../llvm-project/llvm/ \
-  -DLLVM_ENABLE_PROJECTS="clang" \
-  -DLLVM_USE_SANITIZER=OFF \
-  -DCMAKE_BUILD_TYPE="Release" \
-  -DCMAKE_C_COMPILER=/usr/local/bin/afl-clang-fast \
-  -DCMAKE_CXX_COMPILER=/usr/local/bin/afl-clang-fast++ \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DLLVM_TARGETS_TO_BUILD="X86" \
-  -DCMAKE_C_FLAGS="-pthread -L/usr/lib/x86_64-linux-gnu" \
-  -DCMAKE_CXX_FLAGS="-pthread -L/usr/lib/x86_64-linux-gnu" \
-  -DCMAKE_EXE_LINKER_FLAGS="-L/usr/lib/x86_64-linux-gnu" \
-  -DLLVM_BUILD_DOCS="OFF"
-
-# Build only clang 
-ninja clang
 
 # -------------------------------------------------------
 # Step 8: Download and extract reindexed after cmin llvmSS c files
