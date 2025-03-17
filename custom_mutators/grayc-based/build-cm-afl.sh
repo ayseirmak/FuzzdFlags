219f93f9386b0d563bb1199971e46d39e2d1092a
Karine EM <karineven79@gmail.com>
2025-03-17 19:06:28 +0000

    Update build-cm-afl.sh

--- custom_mutators/grayc-based/build-cm-afl.sh
+++ custom_mutators/grayc-based/build-cm-afl.sh
@@ -1,7 +1,7 @@
 pathAFL=$1/include/
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-jm.o -D JUMP_MUTATOR
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-dupm.o -D DUPLICATE_MUTATOR
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-cm.o -D CONSTANT_MUTATOR
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-delm.o -D DELETE_MUTATOR
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-am.o -D ASSIGNMENT_MUTATOR
-gcc -Wall -O3 -I $pathAFL cm-standalone.c -o cm-em.o -D EXPRESSION_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-jm.so -D JUMP_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-dupm.so -D DUPLICATE_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-cm.so -D CONSTANT_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-delm.so -D DELETE_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-am.so -D ASSIGNMENT_MUTATOR
+gcc -Wall -O3 -I $pathAFL cm.c -o cm-em.so -D EXPRESSION_MUTATOR
