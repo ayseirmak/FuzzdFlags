311973b91c8ca0912c926877215f89e6941070f4
AS <user42@node0.fuzzing-issta.kclsystemfuzz-pg0.utah.cloudlab.us>
2025-03-18 08:50:17 -0600

    add Ayse data

--- custom_mutators/grayc-based/build-cm-standalone.sh
+++ custom_mutators/grayc-based/build-cm-standalone.sh
@@ -1,6 +1,6 @@
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-jm.o -D JUMP_MUTATOR
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-dupm.o -D DUPLICATE_MUTATOR
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-cm.o -D CONSTANT_MUTATOR
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-delm.o -D DELETE_MUTATOR
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-am.o -D ASSIGNMENT_MUTATOR
-gcc -Wall -O3 cm-standalone3.c -o cm-standalone-em.o -D EXPRESSION_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-jm.o -D JUMP_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-dupm.o -D DUPLICATE_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-cm.o -D CONSTANT_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-delm.o -D DELETE_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-am.o -D ASSIGNMENT_MUTATOR
+gcc -Wall -O3 cm-standalone4.c -o cm-standalone-em.o -D EXPRESSION_MUTATOR
