0ecf611bef30c7b813be2d0101ac1b8a93b3e400
ayseirmak <152956281+ayseirmak@users.noreply.github.com>
2025-05-31 22:55:36 +0100

    Update 61-backend-cov-analysis.sh

--- 1-LLVMSS-dataset/0-baselines/61-backend-cov-analysis.sh
+++ 1-LLVMSS-dataset/0-baselines/61-backend-cov-analysis.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 working_folder=$1
+table_name=$2
+
 LOGFILE="backend_coverage_analysis.log"
 
 # Redirect all standard output and error to the log file.
@@ -8,21 +10,21 @@ exec > "$working_folder/$LOGFILE" 2>&1
 
 echo "===== STARTING SCRIPT at $(date) ====="
 
-CSV_FILE="$working_folder/table__all_1_0.csv"
+CSV_FILE="$working_folder/$table_name"
 
 declare -a GROUP_PATTERNS=(
   "clang/lib/CodeGen/"
   "llvm/lib/CodeGen/"
-  "llvm/lib/AsmParser"
-  "llvm/lib/Bitcode"
-  "llvm/lib/MC"
-  "llvm/lib/MCA"
-  "llvm/lib/LTO"
-  "llvm/lib/ExecutionEngine"
-  "llvm/lib/Linker"
-  "llvm/lib/DebugInfo"
-  "llvm/lib/Support"
-  "llvm/lib/Target"
+  "llvm/lib/AsmParser/"
+  "llvm/lib/Bitcode/"
+  "llvm/lib/MC/"
+  "llvm/lib/MCA/"
+  "llvm/lib/LTO/"
+  "llvm/lib/ExecutionEngine/"
+  "llvm/lib/Linker/"
+  "llvm/lib/DebugInfo/"
+  "llvm/lib/Support/"
+  "llvm/lib/Target/"
   "llvm/utils/TableGen/"
 )
 
