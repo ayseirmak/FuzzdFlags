681516f7c8d034c5d47449de408bc03fb48a2cc9
ayseirmak <152956281+ayseirmak@users.noreply.github.com>
2025-06-04 21:37:00 +0100

    Update 51-inner-LH_file.sh

--- 1-LLVMSS-dataset/4-coverage/51-inner-LH_file.sh
+++ 1-LLVMSS-dataset/4-coverage/51-inner-LH_file.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-############################
-## MAIN:
-############################
+#!/usr/bin/env bash
+set -euo pipefail
 
-fileO=$1 			# file name originally
-file1=$2 			# file 1
+fileO="$1"
+file1="$2"
 
-file1_ln=`cat $file1 | wc -l` # size file 1
-file1_cov_any=`cat $file1 | awk '{if ($2 >0) print($2)}' | wc -l`
+file1_ln=$(wc -l <"$file1")
+file1_cov_any=$(awk '$2>0 {c++} END{print c+0}' "$file1")
+
 echo ">> $fileO,$file1_ln,$file1_cov_any"
